f732d1893adf7bffc228b9d9f7fa3a782a585950
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-07-08 20:26:33 -0300

    fix in production capistrano settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
-server 'ec2-52-10-164-28.us-west-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{:app :web :db}, primary: true
+server 'ec2-52-10-164-28.us-west-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :rails_env, 'production'
